d2d05e72bd797b1403bf6cd7ddfa134a9b245d5f
Aayushak01 <121002949+Aayushak01@users.noreply.github.com>
2025-10-08 19:29:14 +0000

    datatypes

---
+++ 01_basic/second.js
@@ -0,0 +1,4 @@
+"use strict"; //treat all JS code as newer version
+
+alert("hello aayush"); // cant be used here as we are using node.js for running our js
+
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,22 @@
-console.log("Aayush");
+// console.log("Aayush");
+
+const firstName = "Aayush Kumar Upadhyay";
+// firstName = "Aayush";
+console.log(firstName);
+
+let lastName = "Upadhyay";
+console.log(lastName);
+
+lastName = "Kumar Upadhyay";
+console.log(lastName);
+let x = 3
+let y = 6
+let z = x*y;
+console.log(z);
+/*
+dont use var as it doesnt have any type scope. So preferably use 
+let and const. In const you can't change the value. 
+ */
+w = 5;
+console.log(w);
+console.log(typeof(w));
