8ed1338a330a250c4fb33dfe661ef61f3634079a
Aayushak01 <121002949+Aayushak01@users.noreply.github.com>
2025-10-08 19:59:08 +0000

    More of datatype

--- 01_basic/second.js
+++ 01_basic/second.js
@@ -1,4 +1,16 @@
 "use strict"; //treat all JS code as newer version
 
-alert("hello aayush"); // cant be used here as we are using node.js for running our js
+// alert("hello aayush"); // cant be used here as we are using node.js for running our js
 
+let value = null
+console.log(typeof value);
+
+let valueInNumber = Number(value);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLogIn = "Aayush";
+
+let booleanIsLogIn = Boolean(isLogIn);
+
+console.log(booleanIsLogIn);
