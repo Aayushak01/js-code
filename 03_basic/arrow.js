b891c4791a87c80ec4e77e270c0ce483506b0296
Aayushak01 <121002949+Aayushak01@users.noreply.github.com>
2025-10-13 18:26:01 +0000

    Arrow Function

--- 03_basic/arrow.js
+++ 03_basic/arrow.js
@@ -3,6 +3,13 @@ const user = {
     price : 999,
 
     welcomeMsg : function(){
-        console.log(`${} welcome to website`)
+        // console.log(`${this.username}, welcome to website`);
+        console.log(this);
     }
 }
+
+user.welcomeMsg();
+user.username = "Perfect";
+user.welcomeMsg();
+
+console.log(this);
